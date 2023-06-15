655972ad3e325e9585a6243cd310f2de3c854bd3
mas242 <mattsmith533811@yahoo.com>
2023-06-15 01:57:10 -0600

    removed create-react-app default content. Added in two generate joke buttons each with their own state.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
 
 function App() {
+  const [joke, setJoke] = useState("");
+  const [joke2, setJoke2] = useState("");
+
+  //api call Axios way
+  const getJoke = () => {
+    Axios.get("https://api.chucknorris.io/jokes/random").then((response) => {
+      // console.log(response);
+      setJoke(response.data.value);
+    });
+  };
+
+  //api call fetch method way
+  const getJoke2 = () => {
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setJoke2(data.value);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>CHUCK NORRIS JOKE GENERATOR<br/>by Matthew Smith</p>
+        <div>
+          <button onClick={getJoke}>Get Joke!</button>
+          <br />
+          {joke}
+          <br />
+          <button onClick={getJoke2}>Get Joke 2!</button>
+          <br />
+          {joke2}
+        </div>
       </header>
     </div>
   );
